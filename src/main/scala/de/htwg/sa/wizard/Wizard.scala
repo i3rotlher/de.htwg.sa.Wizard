f2dd72be7ba214d1b1222a4d3f3b9f52bb967fb3
i3rotlher <maxe.kw@googlemail.com>
2021-06-22 09:36:40 +0200

    Docker working

--- src/main/scala/de/htwg/sa/wizard/Wizard.scala
+++ src/main/scala/de/htwg/sa/wizard/Wizard.scala
@@ -1,6 +1,7 @@
 package de.htwg.sa.wizard
 import com.google.inject.Guice
 import de.htwg.sa.wizard.aview.TUI
+import de.htwg.sa.wizard.aview.gui.SwingGUI
 import de.htwg.sa.wizard.control.controllerBaseImpl.{Controller, game_over, game_started}
 
 object Wizard {
@@ -8,7 +9,7 @@ object Wizard {
   val injector = Guice.createInjector(new WizardModule)
   val controller = injector.getInstance(classOf[Controller])
   val tui = new TUI(controller)
-  //val gui = new SwingGUI(controller)
+  val gui = new SwingGUI(controller)
 
   def main(args: Array[String]): Unit = {
     controller.publish(new game_started)
